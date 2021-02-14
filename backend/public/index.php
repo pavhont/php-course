6bb638ab77c6ebd594afe0c75d74c310f3d0e36f
Pavel Hontarenko <pavgont@gmail.com>
2021-02-14 22:42:27 +0200

    Write in file

--- backend/public/index.php
+++ backend/public/index.php
@@ -7,3 +7,29 @@ use \It20Academy\App\Core\App;
 $app = new App();
 
 $app->run();
+
+//========== Запись данных в файл ============
+
+/**
+ * w+ - открытие файла для чтения и записи, помещает указатель в начало файла
+ * и обрезает файл до нулевой длины.
+ * если файл не существует, пытается его создать
+ *
+ * fopen() - Открывает файл или URL
+ * @url https://www.php.net/manual/ru/function.fopen.php
+ *
+ * fwrite() — Бинарно-безопасная запись в файл
+ * @url https://www.php.net/manual/ru/function.fwrite.php
+ *
+ * fclose() — Закрывает открытый дескриптор файла
+ * @url https://www.php.net/manual/ru/function.fclose.php
+ *
+ */
+$file = fopen('./../storage/file.txt', "a+");
+
+if (! $file) {
+    echo 'Ошибка открытия файла';
+} else {
+    fwrite($file, 'Test String' . PHP_EOL);
+    fclose($file);
+}
